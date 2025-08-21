270886ae989bce6d5e73c97027c1daba4e9966f0
YagoLima006 <yagof092@gmail.com>
2025-08-21 17:58:26 -0300

    Criando o projeto e o repositorio

---
+++ src/db/dbExcepetion.java
@@ -0,0 +1,13 @@
+package db;
+
+public class dbExcepetion extends RuntimeException
+{
+
+	private static final long serialVersionUID = 1L;
+	
+	public dbExcepetion(String msg)
+	{
+		super(msg);
+	}
+
+}
